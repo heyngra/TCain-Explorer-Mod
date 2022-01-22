44f45b222278feb47300970a4c1b1524a8539f09
heyngra <heyngra.wspolpraca@gmail.com>
2022-01-22 20:53:44 +0100

    1.2

--- main.lua
+++ main.lua
@@ -9,26 +9,30 @@ function TCAIN:open_url(url)
     end
 end
 
+function TCAIN:url()
+    if Game():GetPlayer(0) ~= Game():GetPlayer(1) then 
+        local notok = pcall(TCAIN:open_url("http://tcain.heyn.live?seed=" .. Seeds.Seed2String(Game():GetSeeds():GetStartSeed()):gsub("%s+", "") .. string.format("%q", "&coop=off")), 0)
+        if notok then
+            Isaac.ConsoleOutput("Relaunch the game with --luadebug to make it work.")
+        end
+    else
+        local notok = pcall(TCAIN:open_url("http://tcain.heyn.live?seed=" .. Seeds.Seed2String(Game():GetSeeds():GetStartSeed()):gsub("%s+", "") .. string.format("%q", "&coop=on")), 0)
+        if notok then
+            Isaac.ConsoleOutput("Relaunch the game with --luadebug to make it work.")
+        end
+    end
+end
+
 function TCAIN:OpenF()
     if (Input.IsButtonTriggered(Keyboard.KEY_F5, 0) and debug) then
-        coop = 0
-        if Game():GetPlayer(0) == Game():GetPlayer(1) then coop = 1 end
-        local ok = pcall(TCAIN:open_url("http://tcain.heyn.live?data=" .. Seeds.Seed2String(Game():GetSeeds():GetStartSeed()):gsub("%s+", "") .. coop))
-        if not ok then
-            Isaac.ConsoleOutput("Relaunch the game with --luadebug to make it work.")
-        end    
+    TCAIN:url()
     end
 end
 
 function TCAIN:OpenWeb(command)
     Isaac.ConsoleOutput(command)
     if command == "open" then
-        coop = 0
-        if Game():GetPlayer(0) == Game():GetPlayer(1) then coop = 1 end
-        local ok = pcall(TCAIN:open_url("http://tcain.heyn.live?data=" .. Seeds.Seed2String(Game():GetSeeds():GetStartSeed()):gsub("%s+", "") .. coop))
-        if not ok then
-            Isaac.ConsoleOutput("Relaunch the game with --luadebug to make it work.")
-        end
+        TCAIN:url()
     end
 end
 
